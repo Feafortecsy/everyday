808c7b83582976ac96949ed37730f0aa76cc9ed2
Feafortecsy <1228256157@qq.com>
2019-08-03 22:48:55 +0800

    webpack Asset Management 1. loading CSS 2. loading images 3. loading fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from "lodash";
+import "./index.css";
+import ShadowImg from "./road_trees_shadow.jpg";
+
 const component = () => {
     /* eslint-disable-next-line no-undef */
     const element = document.createElement("div");
+    const Shadow = new Image();
+    Shadow.src = ShadowImg;
+    element.appendChild(Shadow);
     /* Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的 */
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.innerHTML = _.join(["Hello", "webpack", "I am changed"], " ");
+    element.classList.add("test");
     return element;
 };
 /* eslint-disable-next-line no-undef */
